b9f3508675104758a6ece6e3552d10fd73f2b2be
Douglas Pardim <douglas.pardim@globalsys.com.br>
2025-01-29 16:14:17 -0300

    fix: dashboard run problem

--- src/core/presentation/dashboard/dashboard.module.ts
+++ src/core/presentation/dashboard/dashboard.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
-import { DashboardService } from '@/app/use-cases/dashboard.service';
+import { DashboardService } from '@app/use-cases/dashboard/dashboard.service';
 import { DashboardController } from './dashboard.controller';
-import { PrismaModule } from '@/infra/prisma/prisma.module';
+import { PrismaModule } from '@infra/prisma/prisma.module';
+import { CROP_REPOSITORY } from '@core/domain/repositories/crop-repository.interface';
+import { PrismaCropRepository } from '@core/infra/repositories/prisma/crop.repository';
+import { FARM_REPOSITORY } from '@core/domain/repositories/farm-repository.interface';
+import { PrismaFarmRepository } from '@core/infra/repositories/prisma/farm.repository';
 
 @Module({
   imports: [PrismaModule],
-  providers: [DashboardService],
+  providers: [
+    DashboardService,
+    {
+      provide: CROP_REPOSITORY,
+      useClass: PrismaCropRepository,
+    },
+    {
+      provide: FARM_REPOSITORY,
+      useClass: PrismaFarmRepository,
+    },
+  ],
   controllers: [DashboardController],
 })
 export class DashboardModule {}
